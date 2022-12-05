65f68bbced7f5e7280ad4454ae08718b511a8f18
ruthwik <dbhavanibakka@gmail.com>
2022-12-05 19:58:44 +0530

    var script

--- 01-print.sh
+++ 01-print.sh
@@ -1,1 +1,10 @@
 echo  hello Durga Bhavani Deepika
+
+# Color Syntax
+# echo -e "\e[COLmHello\e[0m"
+# -e - enable \e
+# \e - enable COL
+# COL - COL CODE , 31-26 (31- REd, 32 - Green, 33 - YELLOW, 34 - BLUE , 35 - MAGENTA, 36 - CYAN )
+# 0m - Disable the enabled color
+
+echo -e "\e[35mHello in RED Color\e[0m"
